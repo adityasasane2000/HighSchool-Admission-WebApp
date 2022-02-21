044692ce5b490ca5bbf5fa0b84afc5219523a41e
Atharva Sanjay Meher <meheratharva49@gmail.com>
2022-02-21 16:11:37 +0530

    HSAM- 1 : Add navigation bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,30 @@
 
-import React from 'react';
+// import React from 'react';
 import {Link} from 'react-router-dom';
+import React, { Component } from 'react'; 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Container } from 'react-bootstrap';
+import "../components/css/styles.css"
 
 const  navigation =()=> {
   return(
     <div>
-      <Link to = "/" className='item'>Home</Link>
+      {/* <Link to = "/" className='item'>Home</Link>
       <Link to = "/aboutus" className='item'>About Us</Link>
       <Link to = "/contactus" className='item'>Contact Us</Link>
-      <Link to = "/admission" className='item'>Admission</Link>
+      <Link to = "/admission" className='item'>Admission</Link> */}
+     <Navbar expand="lg">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="nav navbar-nav ">
+        <Nav.Link className='item' id="item" href="#home">Home</Nav.Link>
+        <Nav.Link className='item' id="item" href="#aboutus">About Us</Nav.Link>
+        <Nav.Link className='item' id="item" href="#contactus">Contact Us</Nav.Link>
+        <Nav.Link className='item' id="item" href="#admission">Admission</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  
+</Navbar>
+
     </div>
   );
 }
