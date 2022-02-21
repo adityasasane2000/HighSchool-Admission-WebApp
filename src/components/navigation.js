b75f701463601b3f46ac294ceec810c67c2df5ef
Atharva Sanjay Meher <meheratharva49@gmail.com>
2022-02-21 18:24:42 +0530

    underline nav removed

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,18 +8,19 @@ import "../components/css/styles.css"
 const  navigation =()=> {
   return(
     <div>
-      {/* <Link to = "/" className='item'>Home</Link>
-      <Link to = "/aboutus" className='item'>About Us</Link>
-      <Link to = "/contactus" className='item'>Contact Us</Link>
-      <Link to = "/admission" className='item'>Admission</Link> */}
+    
      <Navbar expand="lg">
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="nav navbar-nav ">
-        <Nav.Link className='item' id="item" href="#home">Home</Nav.Link>
+      <Link to = "/" className='item'  id="item" >Home</Link>
+      <Link to = "/aboutus" className='item'  id="item">About Us</Link>
+      <Link to = "/contactus" className='item'  id="item" >Contact Us</Link>
+      <Link to = "/admission" className='item'  id="item">Admission</Link>
+        {/* <Nav.Link className='item' id="item" href="#home">Home</Nav.Link>
         <Nav.Link className='item' id="item" href="#aboutus">About Us</Nav.Link>
         <Nav.Link className='item' id="item" href="#contactus">Contact Us</Nav.Link>
-        <Nav.Link className='item' id="item" href="#admission">Admission</Nav.Link>
+        <Nav.Link className='item' id="item" href="#admission">Admission</Nav.Link> */}
       </Nav>
     </Navbar.Collapse>
   
