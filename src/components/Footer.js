a66fabf287ca84a1c5741fbb840e8aa2860686c1
Meher Atharva <meheratharva49@gmail.com>
2022-03-22 22:15:35 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import "../components/Footer.css";
+import { Link } from "react-router-dom";
+
 
 function Footer() {
   return (
-    <div className='Footer'>Footer</div>
+    <div className='Footer'>
+      Footer
+      
+      <h6 id='All-Rights'>All Rights Reserved</h6>
+      <Link to="hi 11">Link</Link>
+              <br />
+      <Link to="hi 11">Link</Link>
+              <br />
+
+    </div>
+    
   )
 }
 
