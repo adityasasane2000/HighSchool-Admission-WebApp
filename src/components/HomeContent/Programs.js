b2e534278b2a60d0457b395b2d0329335c3e94c5
Rajesh <pratikmorve2@gmail.com>
2022-04-19 20:20:29 +0530

    programs file change

--- src/components/HomeContent/Programs.js
+++ src/components/HomeContent/Programs.js
@@ -1,61 +1,60 @@
-import React  from 'react'
-import "../../components/HomeContent/Program.css"
-import { Row, Col, Container } from 'react-bootstrap';
+import React from "react";
+import "../../components/HomeContent/Program.css";
+import { Row, Col, Container } from "react-bootstrap";
 
+import { Link } from "react-router-dom";
 
+const Programs = () => {
+  return (
+    <>
+      <Container>
+        <Row md={12}>
+          <h1 className="homepage-headings">PROGRAMS</h1>
+        </Row>
+      </Container>
+      <div className="Programsbox">
+        <Container>
+          <Row>
+            <Col lg={4} md={12}>
+              <div className="cardlayout1">
+                <h3 className="program-titles">Science</h3>
+                <div className="text-box">
+                  <Link to="/science" id="ttvd">
+                    {" "}
+                    Tap to view details
+                  </Link>
+                </div>
+              </div>
+            </Col>
 
+            <Col lg={4} md={12}>
+              <div className="cardlayout2">
+                <h3 className="program-titles">Commerce</h3>
+                <div className="text-box">
+                  <Link to="/commarce" id="ttvd">
+                    {" "}
+                    Tap to view details
+                  </Link>
+                </div>
+              </div>
+            </Col>
 
- const Programs = ()=> {
- 
-  
- return<>
- <Container>
- <Row md={12}>
- <h1 className='homepage-headings'>PROGRAMS</h1>
- </Row>
- </Container>
- <div className='Programsbox'>
- <Container>
- <Row >
-             
-
-
- <Col lg={4} md={12} >
- <div className="cardlayout1">
-     
-        <h3 className='program-titles'>Science</h3>
-        <div className="text-box">
-            <p id='ttvd' >Tap to view details</p>
-        </div>
-    </div>
-</Col>
- 
-
-<Col lg={4} md={12}>
-    <div className="cardlayout2">
-        <h3 className='program-titles'>Commerce</h3>
-        <div className="text-box">
-            <p id='ttvd' > Tap to view details</p>
-        </div>
-    </div>
-    </Col>
-
-    <Col lg={4} md={12}>
-    <div className="cardlayout3">
-        <h3 className='program-titles'> Arts</h3>
-        <div className="text-box">
-            <p id='ttvd'>Tap to view details</p>
-        </div>
-    
-    </div>
-    </Col>
-  
- 
-    </Row>
-    </Container>
-    </div>
-
- </>
-}
+            <Col lg={4} md={12}>
+              <div className="cardlayout3">
+                <h3 className="program-titles"> Arts</h3>
+                <div className="text-box">
+                  <Link to="/art" id="ttvd">
+                    {" "}
+                    Tap to view details
+                  </Link>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
 export default Programs;
